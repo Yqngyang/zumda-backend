34d8901cdf0d00bc2b93d4be4f0fa5705777a0da
Yqngyang <zdnima@gmail.com>
2026-04-14 16:20:26 +0900

    フロントエンドに対してバックエンドの修正

--- config/settings.py
+++ config/settings.py
@@ -78,3 +78,18 @@ REST_FRAMEWORK = {
     "DEFAULT_RENDERER_CLASSES": ["rest_framework.renderers.JSONRenderer"],
     "DEFAULT_PARSER_CLASSES": ["rest_framework.parsers.JSONParser"],
 }
+
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+EMAIL_HOST = "smtp.gmail.com"
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+
+EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER")
+EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
+DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
+
+NOTIFICATION_EMAILS = [
+    email.strip()
+    for email in os.getenv("NOTIFICATION_EMAILS", "").split(",")
+    if email.strip()
+]
